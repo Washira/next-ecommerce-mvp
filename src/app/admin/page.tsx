0f7c89551f8596d4d168e8b7ff19aa74b1426d44
washira <washira.suyajai@gmail.com>
2024-04-22 22:42:37 +0700

    ✨ (admin/page.tsx): Introduce new AdminDashboard component to display total sales data in a card format ✨ (components/ui/card.tsx): Add new reusable Card component with CardHeader, CardTitle, CardDescription, CardContent, and CardFooter subcomponents 📝 (db/db.ts): Add PrismaClient instance creation and export it as db singleton instance 📝 (lib/formatters.ts): Add formatCurrency and formatNumber functions for formatting currency and numbers respectively

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,52 @@
-export default function AdminDashboard({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import db from '@/db/db'
+import { formatNumber } from '@/lib/formatters'
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: {
+      pricePaid: true,
+    },
+    _count: true,
+  })
+  return {
+    amount: (data._sum?.pricePaid || 0) / 100,
+    numberOfSales: data._count,
+  }
+}
+export default async function AdminDashboard() {
+  const salesData = await getSalesData()
   return (
-    <>
-      <h1>Dashboard</h1>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {children}
-      </div>
-    </>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <DashboardCard
+        title="Total Sales"
+        subtitle={formatNumber(salesData.numberOfSales)}
+        body={formatNumber(salesData.amount)}
+      />
+    </div>
+  )
+}
+
+type dashboardCardProps = {
+  title: string
+  subtitle: string
+  body: string
+}
+
+export function DashboardCard({ title, subtitle, body }: dashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>{body}</CardContent>
+    </Card>
   )
 }
